46d2befd4f73da778185dc2a26a08fbcbef32675
AlexKVal <alexkval@gmail.com>
2015-09-01 21:13:09 +0300

    [added] realised first part of pre-bootstrapping initial data
    they are sended within 'script text/x-data'

--- routes/bbApp.js
+++ routes/bbApp.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var todos = require('../bbApp/data');
 
 var router = express.Router();
 
 router.use('/', express.static('bbApp'));
 
 router.get('/', function (req, res) {
-  res.render('bbApp', {myVar: 'world'});
+  res.render('bbApp', {todos: JSON.stringify(todos, null , 2)});
 })
 
 module.exports = router;
