b6c966541b8455ed4303ae8ecee7b72391f696b9
AlexKVal <alexkval@gmail.com>
2015-09-04 21:40:25 +0300

    [changed] convert html->jade template and move it into 'include' jade

--- bbApp/data.js
+++ bbApp/data.js
@@ -1,9 +1,9 @@
 var todos = [
   {id: 1, description: 'Pick up milk.', status: 'incomplete'},
-  {id: 2, description: 'Get a car wash', status: 'complete'},
+  {id: 2, description: 'Get a car wash.', status: 'complete'},
   {id: 3, description: 'Learn Backbone.', status: 'incomplete'},
   {id: 4, description: 'Go watch a movie.', status: 'incomplete'},
-  {id: 5, description: 'Take rest.', status: 'complete'},
+  {id: 5, description: 'Get some rest.', status: 'complete'},
   {id: 6, description: 'Fly to the sky.', status: 'complete'}
 ];
 
