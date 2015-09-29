6836afef4edac9bbae24f2bb93d9700c2ea6a21a
AlexKVal <alexkval@gmail.com>
2015-09-29 16:48:38 +0300

    [added] todos list fetching in 'index' route
    print it to console

--- clients/ember/client.js
+++ clients/ember/client.js
@@ -1,3 +1,30 @@
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
+
+App.ApplicationSerializer = DS.JSONSerializer.extend();
+
+App.Todo = DS.Model.extend({
+  description: DS.attr('string'),
+  // One of ['incomplete', 'complete']
+  status: DS.attr('string')
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    var promise = this.store.findAll('todo');
+
+    // -- debug --
+    promise.then(function(items) {
+      items.forEach(function(item) {
+        var msg = 'Item: id=' + item.id + ' ' +
+          'desc="' + item.get('description') + '" ' +
+          'status="' + item.get('status') + '"';
+        console.log(msg);
+      })
+    })
+    // ----------
+
+    return promise;
+  }
+});
